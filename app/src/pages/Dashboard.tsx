d662fb07bad8b9842f17b9fba14df9364c30a915
Ashish Dhakal <114866470+found-sec@users.noreply.github.com>
2025-01-15 18:48:02 +0300

    Update Dashboard.tsx

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ import {
 	Link,
 } from "@chakra-ui/react";
 import PortfolioPreview from "../components/PortfolioPreview";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PositionsList from "../components/PositionsList";
 import Newsfeed from "../components/Newsfeed";
 import Watchlist from "../components/Watchlist";
@@ -28,7 +28,6 @@ export default function Dashboard() {
 			dateRange: "12M",
 			showChart: true,
 			locale: "en",
-			largeChartUrl: "",
 			isTransparent: true,
 			showSymbolLogo: true,
 			showFloatingTooltip: true,
@@ -40,8 +39,6 @@ export default function Dashboard() {
 			scaleFontColor: "rgba(34, 211, 238, 1)",
 			belowLineFillColorGrowing: "rgba(34, 211, 238, 0.12)",	
 			belowLineFillColorFalling: "rgba(34, 211, 238, 0.12)",
-			belowLineFillColorGrowingBottom: "rgba(34, 211, 238, 0)",
-			belowLineFillColorFallingBottom: "rgba(34, 211, 238, 0)",
 			symbolActiveColor: "rgba(34, 211, 238, 0.12)",
 			tabs: [
 				{
@@ -50,7 +47,6 @@ export default function Dashboard() {
 						{ s: "FOREXCOM:SPXUSD", d: "S&P 500 Index" },
 						{ s: "FOREXCOM:DJI", d: "Dow Jones Industrial Average Index" },
 					],
-					originalTitle: "Indices",
 				},
 			],
 		});
@@ -60,12 +56,14 @@ export default function Dashboard() {
 			overviewContainer.appendChild(overviewScript);
 		}
 
-		//   return () => {
-		// 	if (overviewContainer) {
-		// 		overviewContainer.removeChild(overviewContainer)   -- uncomment this if you want the website to break
-		// 	}
-		// };
-
+		return () => {
+			if (overviewContainer) {
+				while (overviewContainer.firstChild) {
+					overviewContainer.removeChild(overviewContainer.firstChild);
+				}
+			}
+		};
+		
 	}, []);
 
 	return (
@@ -104,49 +102,49 @@ export default function Dashboard() {
 							</Text>
 							{/* TradingView Widget */}
 							<Box className="tradingview-widget-container" id="tradingview-market-overview-widget">
-								<Box className="tradingview-widget-container__widget"></Box>
-							</Box>
-						</>
-					)}
-					{!isOnMobile && (
-						<>
-							<Spacer height={10} />
-							<Heading size="md">Stock Market News</Heading>
-							<Spacer height={2} />
-							<Newsfeed symbol={""} />
-						</>
-					)}
-				</Box>
-				<Box
-					flex="0.25"
-					borderWidth={{ base: 0, md: 1 }}
-					borderRadius="md"
-					p={{ base: 0, md: 3 }}
-					height={"fit-content"}
-				>
-					{tokens.isAuthenticated() ? (
-						<>
-							<PositionsList />
-							<Spacer h="3" />
-							<Watchlist />
-						</>
-					) : (
-						<Box>
-							<Heading as="h6" size="xs" textAlign={"center"}>
-								Sign in to view positions and watchlist
-							</Heading>
-						</Box>
-					)}
-				</Box>
-			</Flex>
-			{isOnMobile && (
-				<>
-					<Spacer height={10} />
-					<Heading size="md">Stock Market News</Heading>
-					<Spacer height={2} />
-					<Newsfeed symbol={""} />
-				</>
-			)}
-		</Box>
-	);
+							 <Box className="tradingview-widget-container__widget"></Box>
+						 </Box>
+					 </> 
+				 )}
+				 {!isOnMobile && (
+					 <>
+					 <Spacer height={10} />
+					 <Heading size="md">Stock Market News</Heading>
+					 <Spacer height={2} />
+					 <Newsfeed symbol={""} />
+					 </>
+				 )}
+			 </Box>
+			 <Box
+				 flex="0.25"
+				 borderWidth={{ base: 0, md: 1 }}
+				 borderRadius="md"
+				 p={{ base: 0, md: 3 }}
+				 height={"fit-content"}
+			 >
+				 {tokens.isAuthenticated() ? (
+					 <>
+					 <PositionsList />
+					 <Spacer h="3" />
+					 <Watchlist />
+					 </>
+				 ) : (
+					 <Box>
+					 <Heading as="h6" size="xs" textAlign={"center"}>
+						 Sign in to view positions and watchlist
+					 </Heading>
+					 </Box>
+				 )}
+			 </Box>
+		 </Flex>
+		 {isOnMobile && (
+			 <>
+			 <Spacer height={10} />
+			 <Heading size="md">Stock Market News</Heading>
+			 <Spacer height={2} />
+			 <Newsfeed symbol={""} />
+			 </>
+		 )}
+	  </Box>
+   );
 }
