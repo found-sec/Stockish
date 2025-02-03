82a51c165169f4b2714970bf517283bad94552e0
Ashish Dhakal <114866470+found-sec@users.noreply.github.com>
2025-02-03 14:48:36 +0300

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "https://stockish-backend.onrender.com", // Your production backend URL
+        target: "http://loclahost:3010", // Your production backend URL
         changeOrigin: true,  // Set to true to change the origin header for external API
         secure: true,        // Set to true for production (if your API uses HTTPS with a valid certificate)
         ws: false,            // Optional, only if using WebSockets
